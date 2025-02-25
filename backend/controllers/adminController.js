44d74f03dc31e877cbd14550e1f7f7363d71ab13
AishwaryaParihar1 <aish7parihar@gmail.com>
2025-02-25 19:57:43 +0530

    fixes for deployment

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 const User = require('../models/client/UserModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { generateAuthToken } = require('../utils/authToken');
 
 exports.adminLogin = async (req, res) => {
